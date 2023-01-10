fd98c56dcb9728780fe9a36ae38de9e209155645
Fernando Carlos <111884680+Solarck@users.noreply.github.com>
2023-01-10 19:56:44 -0300

    upload codigo js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
 function TocaSomPom() {
-    document.querySelector('#som_tecla_pom').play
-    console.log('teste')
+    document.querySelector('#som_tecla_pom').play()
+}
+
+let contador = 0
+
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = TocaSomPom
+    contador = contador + 1
+    console.log(contador)
 }
