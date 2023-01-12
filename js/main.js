e7017ff99192e2cb99f7843f8fa4269346a14275
Fernando Carlos <111884680+Solarck@users.noreply.github.com>
2023-01-12 13:32:52 -0300

    novas funções

--- js/main.js
+++ js/main.js
@@ -9,11 +9,8 @@ let contador = 0
 while (contador < listaDeTeclas.length) {
 
     const tecla = listaDeTeclas[contador]
-
     let instrumento = tecla.classList[1]
-
     const idAudio = `#som_${instrumento}`
-    console.log(idAudio)
 
     listaDeTeclas[contador].onclick = function () {
         TocaSom(idAudio)
@@ -21,5 +18,14 @@ while (contador < listaDeTeclas.length) {
 
     contador = contador + 1
 
-    console.log(contador)
+    tecla.onkeydown = function (evento) {
+        // console.log(evento.code == 'Space')=
+        if (evento.code == 'Space' || evento.code == 'Enter') {
+            tecla.classList.add('ativa')
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
+    }
 }
